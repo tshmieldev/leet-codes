58d19ad079c580be14bbc9d5c2c2cf2d86116047
tshmieldev <wojtekrok88@gmail.com>
2025-09-02 21:45:54 +0200

    more problems

---
+++ 145 - binary tree postorder traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        arr = []
+        def dfs(node):
+            if node == None:
+                return
+            dfs(node.left)
+            dfs(node.right)
+            arr.append(node.val)
+        dfs(root)
+        return arr
