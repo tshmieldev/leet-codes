bc78665fa38f0428fb944664afa46472e2beb1cd
tshmieldev <wojtekrok88@gmail.com>
2025-09-12 00:28:09 +0200

    more problems

---
+++ 3110 - score of a string.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        total = 0
+        
+        for i in range(len(s) - 1):
+            total += abs(ord(s[i]) - ord(s[i+1]))
+        
+        return total
