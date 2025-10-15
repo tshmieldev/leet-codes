4bf0d33b577923d9ca0635b24878c8fa729bff48
tshmieldev <wojtekrok88@wp.pl>
2025-10-15 19:46:05 +0200

    wrong version

--- 3349 - adjacent increasing subarrays detection I.py
+++ 3349 - adjacent increasing subarrays detection I.py
@@ -1,6 +1,13 @@
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
-        for i in range(1, len(nums) - 2):
-            if nums[i] > nums[i-1] and nums[i+1] < nums[i] and nums[i+2] > nums[i+1]:
+        def isIncreasing(arr):
+            if len(arr) < 2:
+                return True
+            for i in range(1, len(arr)):
+                if arr[i] <= arr[i-1]:
+                    return False
+            return True
+        for i in range(len(nums)-k*2+1):
+            if isIncreasing(nums[i:k+i]) and isIncreasing(nums[k+i:k+i+k]):
                 return True
         return False
